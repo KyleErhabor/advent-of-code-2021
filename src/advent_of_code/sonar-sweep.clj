cac7a1956fa338911cb07f3fa7b6440b6270dd5d
Kyle Erhabor <kyleerhabor@gmail.com>
2021-12-01 16:19:01 -0500

    Add README and simplify sonar sweep solution

--- src/advent_of_code/sonar-sweep.clj
+++ src/advent_of_code/sonar-sweep.clj
@@ -2,7 +2,7 @@
   (:require [clojure.string :as str]))
 
 (defn sonar-sweep []
-  (->> (str/split (slurp "resources/sonar-sweep.txt") #"\n")
+  (->> (str/split-lines (slurp "resources/sonar-sweep.txt"))
        (map #(Long/parseLong %))
        (partition 2 1)
        (filter (partial apply <))
