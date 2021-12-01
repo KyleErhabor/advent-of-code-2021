b21d9b0661bc3a3616a32a8280046e38c67700b9
Kyle Erhabor <kyleerhabor@gmail.com>
2021-12-01 16:49:15 -0500

    Add second solution

--- src/advent_of_code/sonar-sweep.clj
+++ src/advent_of_code/sonar-sweep.clj
@@ -1,9 +1,19 @@
 (ns advent-of-code.sonar-sweep
   (:require [clojure.string :as str]))
 
-(defn sonar-sweep []
-  (->> (str/split-lines (slurp "resources/sonar-sweep.txt"))
-       (map #(Long/parseLong %))
+(defn nums []
+  (map #(Long/parseLong %) (str/split-lines (slurp "resources/sonar-sweep.txt"))))
+
+(defn part1 []
+  (->> (nums)
        (partition 2 1)
        (filter (partial apply <))
        count))
+
+(defn part2 []
+  (->> (nums)
+       (partition 3 1)
+       (map (partial apply +))
+       (partition 2 1)
+       (filter (partial apply <))
+       (count)))
