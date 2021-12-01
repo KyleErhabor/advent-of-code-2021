0166a485706bc3f093a0cf51f90da0a5b9b7598d
Kyle Erhabor <kyleerhabor@gmail.com>
2021-12-01 17:53:45 -0500

    Rename function

--- src/advent_of_code/sonar-sweep.clj
+++ src/advent_of_code/sonar-sweep.clj
@@ -3,7 +3,7 @@
 
 ;; Tonsky's solution is even cooler: https://github.com/tonsky/advent2021/blob/main/src/advent2021/day1.clj
 
-(defn nums []
+(defn data []
   (map #(Long/parseLong %) (str/split-lines (slurp "resources/sonar-sweep.txt"))))
 
 (defn solve [nums]
@@ -13,10 +13,10 @@
        count))
 
 (defn part1 []
-  (solve (nums)))
+  (solve (data)))
 
 (defn part2 []
-  (->> (nums)
+  (->> (data)
        (partition 3 1)
        (map (partial apply +))
        solve))
